a4380a4a052c3896e0f554eb87c0077d973b0d68
Fisher5991 <fisher5991@gmail.com>
2017-10-25 03:38:34 +0300

    Оптимизировать векторную графику (изменения)

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (navMain.classList.contains("main-nav--opened")) {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
+  } else {
+    navMain.classList.add("main-nav--opened");
+    navMain.classList.remove("main-nav--closed");
+  }
+});
